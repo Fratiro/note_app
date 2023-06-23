fa0bbc4443eb6fbadf7a0bba65db4c7785852985
fratiro <feras.el.mokdad@gmail.com>
2023-06-23 14:02:09 +0300

    create hive auto generate file

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,7 +1,18 @@
-class NoteModel {
+import 'package:hive/hive.dart';
+part 'note_model.g.dart';
+
+
+
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject {
+
+  @HiveField(0)
   final String noteTitle;
+  @HiveField(1)
   final String noteDescription;
+  @HiveField(2)
   final String dateTime;
+  @HiveField(3)
   final int colors;
 
   NoteModel({
