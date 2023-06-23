4bf49a4f8e7a9c1cfcdea53052217356b0365b14
fratiro <feras.el.mokdad@gmail.com>
2023-06-23 13:37:01 +0300

    add Note Model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,13 @@
+class NoteModel {
+  final String noteTitle;
+  final String noteDescription;
+  final String dateTime;
+  final int colors;
+
+  NoteModel({
+    required this.noteTitle,
+    required this.noteDescription,
+    required this.dateTime,
+    required this.colors,
+  });
+}
