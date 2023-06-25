3aa39a165508ce468a8e9757c4f3339fc7a562c7
fratiro <feras.el.mokdad@gmail.com>
2023-06-26 00:27:10 +0300

    Add bloc consumer to bottom sheet

---
+++ lib/views/widgets/bottom_sheet_form.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+import 'custom_button_widget.dart';
+import 'custom_text_field.dart';
+
+class BottomSheetForm extends StatefulWidget {
+  const BottomSheetForm({
+    super.key,
+  });
+
+  @override
+  State<BottomSheetForm> createState() => _BottomSheetFormState();
+}
+
+class _BottomSheetFormState extends State<BottomSheetForm> {
+  final GlobalKey<FormState> formKey = GlobalKey();
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+  String? noteTitle, noteDesription;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      autovalidateMode: autovalidateMode,
+      child: Column(
+        children: [
+          CustomTextField(
+            onSaved: (value) {
+              noteTitle = value;
+            },
+            hint: "Add Note Title",
+            validator: (value) {
+              if (value?.isEmpty ?? true) {
+                return "Sorry this field is required please add a note title";
+              } else {
+                return null;
+              }
+            },
+          ),
+          const SizedBox(height: 16),
+          CustomTextField(
+            hint: "Add your note here....",
+            maxLine: 5,
+            validator: (value) {
+              if (value?.isEmpty ?? true) {
+                return "Sorry this field is required please add your note";
+              } else {
+                return null;
+              }
+            },
+            onSaved: (value) {
+              noteDesription = value;
+            },
+          ),
+          const SizedBox(height: 52),
+          CustomButton(
+            buttonOnPress: () {
+              if (formKey.currentState!.validate()) {
+                formKey.currentState!.save();
+              } else {
+                autovalidateMode = AutovalidateMode.always;
+                setState(() {});
+              }
+            },
+            buttonWidth: MediaQuery.of(context).size.width,
+            buttonHeight: 44,
+            backgroundColor: Colors.yellow.shade200,
+            buttonText: "Add Your Note",
+            textColor: Colors.black,
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/views/widgets/bottom_sheet_note_widget.dart
+++ lib/views/widgets/bottom_sheet_note_widget.dart
@@ -1,87 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:note_app/views/widgets/custom_text_field.dart';
-import 'custom_button_widget.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:note_app/cubits/add_note_cubit/add_note_cubit.dart';
+import 'bottom_sheet_form.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 32),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 32),
       child: SingleChildScrollView(
-        child: BottomSheetForm(),
-      ),
-    );
-  }
-}
-
-class BottomSheetForm extends StatefulWidget {
-  const BottomSheetForm({
-    super.key,
-  });
-
-  @override
-  State<BottomSheetForm> createState() => _BottomSheetFormState();
-}
-
-class _BottomSheetFormState extends State<BottomSheetForm> {
-  final GlobalKey<FormState> formKey = GlobalKey();
-  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
-  String? noteTitle, noteDesription;
-
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      key: formKey,
-      autovalidateMode: autovalidateMode,
-      child: Column(
-        children: [
-          CustomTextField(
-            onSaved: (value) {
-              noteTitle = value;
-            },
-            hint: "Add Note Title",
-            validator: (value) {
-              if (value?.isEmpty ?? true) {
-                return "Sorry this field is required please add a note title";
-              } else {
-                return null;
-              }
-            },
-          ),
-          const SizedBox(height: 16),
-          CustomTextField(
-            hint: "Add your note here....",
-            maxLine: 5,
-            validator: (value) {
-              if (value?.isEmpty ?? true) {
-                return "Sorry this field is required please add your note";
-              } else {
-                return null;
-              }
-            },
-            onSaved: (value) {
-              noteDesription = value;
-            },
-          ),
-          const SizedBox(height: 52),
-          CustomButton(
-            buttonOnPress: () {
-              if (formKey.currentState!.validate()) {
-                formKey.currentState!.save();
-              } else {
-                autovalidateMode = AutovalidateMode.always;
-                setState(() {});
-              }
-            },
-            buttonWidth: MediaQuery.of(context).size.width,
-            buttonHeight: 44,
-            backgroundColor: Colors.yellow.shade200,
-            buttonText: "Add Your Note",
-            textColor: Colors.black,
-          )
-        ],
+        child: BlocConsumer<AddNoteCubit, AddNoteState>(
+          listener: (context, state) {
+            if (state is AddNoteSuccess) {
+              Navigator.pop(context);
+            }
+          },
+          builder: (context, state) {
+            return ModalProgressHUD(
+              inAsyncCall: state is AddNoteLoading ? true : false,
+              child: const BottomSheetForm(),
+            );
+          },
+        ),
       ),
     );
   }
