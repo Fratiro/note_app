2f4e616f0e66f7103923987ce4aa8ac1597108d0
fratiro <feras.el.mokdad@gmail.com>
2023-06-26 01:15:44 +0300

    to be contin/

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:note_app/views/widgets/notes_body_widget.dart';
-
 import 'widgets/bottom_sheet_note_widget.dart';
 
 class NotesView extends StatelessWidget {
